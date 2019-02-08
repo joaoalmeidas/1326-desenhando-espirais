ba27cdf64e57c8588a02e2c8d92c038955c18628
joao <joao182cx@gmail.com>
2019-02-08 18:15:26 -0200

    Criação do loop que define os pontos do espiral.

--- src/EspiralPanel.java
+++ src/EspiralPanel.java
@@ -13,13 +13,57 @@ public class EspiralPanel extends JPanel {
 		int[] pontosX = new int[18];
 		int[] pontosY = new int[18];
 		
-		pontosX[0] = getWidth()/2;
-		pontosY[0] = getWidth()/2;
+		int pulo = 10;
 		
-		for(int i = 0; i < pontosX.length; i++) {
+		pontosX[0] = getWidth()/2;
+		pontosY[0] = getHeight()/2;
+		
+		for(int i = 1; i < pontosX.length; i++) {
 			
+			if(i%8 == 1) {
+				
+				pontosX[i] = pontosX[i - 1] - 10;
+				pontosY[i] = pontosY[i - 1] +  pulo;
+				
+			}else if(i%8 == 2) {
+				
+				pontosX[i] = pontosX[i - 1] - pulo;
+				pontosY[i] = pontosY[i - 1] +  10;
+				
+			}else if(i%8 == 3) {
+				
+				pontosX[i] = pontosX[i - 1] - pulo;
+				pontosY[i] = pontosY[i - 1] - 10;
+				
+			}else if(i%8 == 4) {
+				
+				pontosX[i] = pontosX[i - 1] - 10;
+				pontosY[i] = pontosY[i - 1] - pulo;
+				
+			}else if(i%8 == 5) {
+				
+				pontosX[i] = pontosX[i - 1] + 10;
+				pontosY[i] = pontosY[i - 1] - pulo;
+				
+			}else if(i%8 == 6) {
+				
+				pontosX[i] = pontosX[i - 1] + pulo;
+				pontosY[i] = pontosY[i - 1] - 10;
+				
+			}else if(i%8 == 7) {
+				
+				pontosX[i] = pontosX[i - 1] + pulo;
+				pontosY[i] = pontosY[i - 1] +10;
+				
+			}else if(i%8 == 0) {
+				
+				pontosX[i] = pontosX[i - 1] - 10;
+				pontosY[i] = pontosY[i - 1] + pulo;
+				
+			}
+
 			
-			
+			pulo+=10;
 			
 		}
 		
